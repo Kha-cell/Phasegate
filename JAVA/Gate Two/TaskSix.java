44eb3d0d20d873a34e4f361498bb873e847243e6
kha-cell <adekanbikhalid62@gmail.com>
2026-03-17 11:46:50 +0100

    update

---
+++ JAVA/Gate Two/TaskSix.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TaskSix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number ");
+	int num = scanner.nextInt();
+	int result = num  +   num;
+	System.out.println(result);
+       }
+   }
