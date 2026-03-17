b4d3703d29ded549504c55154745deebb5b73ca0
kha-cell <adekanbikhalid62@gmail.com>
2026-03-17 11:38:20 +0100

    still on it

---
+++ Python/Gate Two/task_seven.py
@@ -0,0 +1,13 @@
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+	num3= float(input("Enter third  number: "))
+        num4 = float(input("Enter fourth number: "))
+	num5 = float(input("Enter fifth number: "))
+        
+
+
+       
+        sum = num1 + num2 + num3 + num4 + num5
+
+       
+        print(f"{num1} + {num2} = {sum}")
---
+++ Python/Gate Two/task_three.py
@@ -0,0 +1,2 @@
+for count in range ( 2 ,5 , 2 ):
+ print(count,end = "   ")
