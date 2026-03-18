792cc4cb01fb0ea853c12801ba25fffb6259055b
kha-cell <adekanbikhalid62@gmail.com>
2026-03-18 13:14:42 +0100

    update

---
+++ JAVA/Gate Three/TaskOne.java
@@ -0,0 +1,8 @@
+public class TaskOne {
+function TaskOne(arr, length, defaultValue = -1) {
+  return arr.concat(Array(length - arr.length).fill(defaultValue));
+     }
+}
+                                                                                                                                                                                                                             
+
+
